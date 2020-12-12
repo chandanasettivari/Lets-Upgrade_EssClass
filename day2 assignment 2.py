7e67de46a7bfc2d84a6f4874fe386a5e8a729916
chandanasettivari <75740142+chandanasettivari@users.noreply.github.com>
2020-12-12 22:33:29 +0530

    Add files via upload

---
+++ day2 assignment 2.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+cars=['BMW',"maruti","volvo"]
+cars.sort()
+cars
+
+
+# In[4]:
+
+
+fruits = ['apple', 'banana', 'cherry']
+
+cars = ['Ford', 'BMW', 'Volvo']
+
+fruits.extend(cars)
+fruits
+
+
+# In[5]:
+
+
+fruits = ['apple', 'banana', 'cherry']
+
+fruits.pop(1)
+fruits
+
+
+# In[6]:
+
+
+fruits = ['apple', 'banana', 'cherry']
+
+fruits.reverse()
+fruits
+
+
+# In[7]:
+
+
+fruits = ['apple', 'banana', 'cherry', 'orange']
+
+fruits.clear()
+fruits
+
+
+# In[ ]:
+
+
+
+
